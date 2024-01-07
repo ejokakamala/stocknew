c98c1ad15117e477ae4ae147c7f75c440dd2a23d
Emmanuel Kamala <emmanuelkamala86@gmail.com>
2024-01-07 15:39:56 +0300

    Edit rails

--- app/models/fixed_expense.rb
+++ app/models/fixed_expense.rb
@@ -1,2 +1,6 @@
 class FixedExpense < ApplicationRecord
+
+  def total
+    cost.to_i * quantity.to_i
+  end
 end
--- app/models/income.rb
+++ app/models/income.rb
@@ -1,3 +1,8 @@
 class Income < ApplicationRecord
   belongs_to :batch
+
+  
+  def amount
+    unit_price.to_i * quantity.to_i
+  end
 end
