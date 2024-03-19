c06890b13de3315fd549df3b66fc59012cd04b9f
ejokakamala <ejokakamala@gmail.com>
2024-03-20 02:17:56 +0300

    Edit bin

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,8 +6,8 @@ bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
 
-ActiveRecord::Base.connection.tables.each do |income| 
-  ActiveRecord::Base.connection.reset_pk_sequence!(income)
+ActiveRecord::Base.connection.tables.each do |incomes| 
+  ActiveRecord::Base.connection.reset_pk_sequence!(incomes)
 end
 
 #if you have seeds to run add:
