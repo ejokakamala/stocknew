1b1681cd643ed732f7e6134ba7cf0a2c0f4b499c
ejokakamala <ejokakamala@gmail.com>
2024-04-03 12:26:16 +0300

    Edit styles

--- app/helpers/incomes_helper.rb
+++ app/helpers/incomes_helper.rb
@@ -1,2 +1,5 @@
 module IncomesHelper
+  def format_date(date, format = :long)
+    date.blank? ? '' : date.to_s(format)
+  end
 end
--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,6 +4,6 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-rails db:drop RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1
-rails db:create RAILS_ENV=production
+# rails db:drop RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+# rails db:create RAILS_ENV=production
 bundle exec rails db:migrate
