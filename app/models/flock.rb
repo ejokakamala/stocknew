97ec26b02650c7e1de3ace80d984553a458c7cc2
ejokakamala <ejokakamala@gmail.com>
2024-05-28 02:24:50 +0300

    Edit flock

--- app/models/flock.rb
+++ app/models/flock.rb
@@ -3,9 +3,7 @@ require 'csv'
 class Flock < ApplicationRecord
   paginates_per 10
 
-  validates :date_in, presence: true
   validates :initial_stock, presence: true
-  validates :retirement_date, presence: true
 
   belongs_to :batch
   belongs_to :user
