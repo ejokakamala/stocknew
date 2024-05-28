70739e88105461d12518a56262062923e7dd0640
ejokakamala <ejokakamala@gmail.com>
2024-05-28 14:22:24 +0300

    Edit batch model

--- db/migrate/20240525080751_add_user_to_batches.rb
+++ db/migrate/20240525080751_add_user_to_batches.rb
@@ -1,5 +1,5 @@
 class AddUserToBatches < ActiveRecord::Migration[7.1]
   def change
-    add_reference :batches, :user, null: false, default: 2, foreign_key: true
+    add_reference :batches, :user, null: false, default: 1, foreign_key: true
   end
 end
