64649047cfb88895475bb429b06b8dec991c74d4
ejokakamala <ejokakamala@gmail.com>
2024-06-08 07:19:40 +0300

    Edit rake task

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,9 +1,9 @@
 # exit on error
 set -o errexit
 
-# bundle install
-# bundle exec rails assets:precompile
-# bundle exec rails assets:clean
+bundle install
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
 # rails db:drop RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 # rails db:create RAILS_ENV=production
 # bundle exec rails db:migrate
